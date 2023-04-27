167d9ff7f9273c37c8e54f5f7a36bb5241deac8c
Marcos Túlio <marcostfandrade@proton.me>
2023-04-27 16:44:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,54 +1,73 @@
+// constantes necessarias para operar o DOM
 const inputs = document.querySelectorAll('input');
 const textarea = document.getElementsByTagName('textarea')[0];
 const label_mensagem = document.getElementById('msg_lb');
 
+// Quando a area do texto for focada o label recebe a classe
 textarea.addEventListener('focus', () => {
     if (textarea.value === '') {
         label_mensagem.classList.add('active');
     }
   });
 
+  // Quando a area do texto for desfocada remove a classe do label
   textarea.addEventListener('blur', () => {
     if (textarea.value === '') {
         label_mensagem.classList.remove('active');
     }
   });
 
+  // Quando digitar na area do texto remove a classe do elemento textarea
+  textarea.addEventListener('input', () => {
+    const erro = document.getElementsByClassName('span');
+    // for para selecionar os elementos especificos com [i]
+    for (let i = 0; inputs.length-1 > i; i++) {
+      erro[i].innerHTML = ""
+      textarea.classList.remove('bred');
+      textarea.nextElementSibling.classList.remove('red'); //temerro
+    }
+});
 
-
+//forEach para percorrer a constante inputs com todos os elementos input
 inputs.forEach(input => {
+  //selecionar todos os labels dos inputs
   const label = input.nextElementSibling;
 
+  //Quando qualquer input for focado o label recebe a classe
   input.addEventListener('focus', () => {
     if (input.value === '') {
       label.classList.add('active');
     }
   });
 
+  //Quando qualquer input for desfocado remove recebe a classe do label 
   input.addEventListener('blur', () => {
     if (input.value === '') {
       label.classList.remove('active');
     }
   });
 
+    // Quando digitar em qualquer input remove a classe do elemento textarea
     input.addEventListener('input', () => {
       const erro = document.getElementsByClassName('span');
-      for (let i = 0; inputs.length-1 > i; i++) {
+      // for para selecionar os elementos especificos com [i]
+      for (let i = 0; inputs.length-2 > i; i++) {
         erro[i].innerHTML = ""
         inputs[i].classList.remove('bred');
-        inputs[i].nextElementSibling.classList.remove('red'); //temerro
+        inputs[i].nextElementSibling.classList.remove('red');
       }
   });
 
-  inputs[2].addEventListener("input", function() {
-    this.value = this.value.replace(/[^0-9\.\-]/g, "");
-  });
+
 });
 
 
+// selecionando o formulário
 const form = document.getElementById("form");
 
+//adicionando envento de submit no formulario
 form.addEventListener("submit", (e) => {
+  //prevenindo o comportamento padrao para fazer validacoes
   e.preventDefault();
 
   const nome = document.getElementById("nome");
@@ -64,29 +83,11 @@ form.addEventListener("submit", (e) => {
       if (mensagem.value.length > 0) {
 
         form.submit();
-
-        /*
-        console.log(`Formulário:\nNome: ${nome.value}\nE-mail: ${email.value}\nCPF: ${cpf.value}`);
-
-        form.classList.add('hidden');
-        
-        const div = document.createElement('div')
-        div.classList.add('post');
-        document.body.appendChild(div);
-        div.innerHTML =
-          `<h2>Formulário enviado</h2>
-            <button type="button" value="Voltar" id="btn">Voltar</button>
-          </script>
-          `
-            btn.addEventListener('click', () => {
-              location.reload();
-            });
-        */
         
       } else {
-        cpf.classList.add('bred');
-        cpf.nextElementSibling.classList.add('red');
-        erro[2].innerHTML = "Digite um CPF válido.";
+        mensagem.classList.add('bred');
+        mensagem.nextElementSibling.classList.add('red');
+        erro[2].innerHTML = "Digite sua mensagem.";
       }
 
     } else {
